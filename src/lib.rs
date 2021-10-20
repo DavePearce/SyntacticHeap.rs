11cae886b46a43bf27a5f278eee480ef853cccc2
DavePearce <david.pearce@ecs.vuw.ac.nz>
2021-10-20 20:45:36 +1300

    Add initial code

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use std::vec;
+
+// =============================================================================
+// SyntacticHeap
+// =============================================================================
+
+pub struct Node<T> {
+    kind: T,
+    children: Vec<usize>
+}
+
+pub struct SyntacticHeap<T> {
+    nodes: Vec<Node<T>>
+}
+
+impl<T> SyntacticHeap<T> {
+    pub fn new() -> SyntacticHeap<T> {
+	SyntacticHeap{nodes: Vec::new()}
+    }
+    /// Determine how many nodes are in this AST.
+    pub fn len(&self) -> usize {
+	self.nodes.len()
+    }
+    /// Access a given node
+    pub fn get(&self, index: usize) -> &Node<T> {
+	&self.nodes[index]
+    }
+    /// Push a new node onto the tree
+    pub fn push(&mut self, kind: T, children: &[usize]) -> usize {
+	// Save current size of tree
+	let idx = self.nodes.len();
+	// Push new node in place	
+	self.nodes.push(Node{kind,children:children.to_vec()});
+	// Return its index
+	idx
+    }
+}
+
+// =============================================================================
+// Tests
+// =============================================================================
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
