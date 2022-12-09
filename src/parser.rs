b437c25b935b7e84d9b8c2ab08463ac8aa2e8311
DavePearce <dave01001110@gmail.com>
2022-12-09 18:26:42 +1300

    Add lists example
    We now have two examples: one for matching braces, and the other for
    lists.

--- src/parser.rs
+++ src/parser.rs
@@ -1,17 +1,21 @@
 use crate::SyntacticHeap;
 
 // ================================================
-// Grammar
+// Parser
 // ================================================
+type Rule<T> = fn(&[T])->Option<(usize,T)>;
 
-// A grammar is itself a syntactic heap.
-pub struct Grammar<T> {
-    reduce: fn(&[T])->Option<(usize,T)>
+/// A generic bottom up parser (i.e. based around shift-reduce
+/// semantics).  In essence, it applies a given reduction rule as much
+/// as possible in a left-to-right order.  This does not currently
+/// support lookahead.
+pub struct Parser<T> {
+    reduce: Rule<T>
 }
 
-impl<T:Copy> Grammar<T> {
-    pub fn new(r: fn(&[T])->Option<(usize,T)>) -> Self {
-	Grammar{reduce: r}
+impl<T:Clone> Parser<T> {
+    pub fn new(r: Rule<T>) -> Self {
+	Self{reduce: r}
     }
     // A very simple reduction
     pub fn parse(&self, input: &[T]) -> Vec<T> {
@@ -20,17 +24,22 @@ impl<T:Copy> Grammar<T> {
 	// Iterate over input stream
 	for i in 0..input.len() {
 	    // Shift
-	    stack.push(input[i]);	    
-	    // Reduce
-	    let r = (self.reduce)(&stack);
-	    // Decide what to do.
-	    match r {
-		Some((n,t)) => {
-		    stack.truncate(stack.len()-n);
-		    stack.push(t);
+	    stack.push(input[i].clone());	    
+	    // Reduce for as along as possible.
+	    loop {
+		// Reduce
+		let r = (self.reduce)(&stack);
+		// Decide what to do
+		match r {
+		    Some((n,t)) => {
+			stack.truncate(stack.len()-n);
+			stack.push(t);
+		    }
+		    None => {
+			break
+		    }
 		}
-		None => {}
-	    }	    
+	    }
 	}
 	stack
     }
--- tests/braces.rs
+++ tests/braces.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 use syntactic_heap::SyntacticHeap;
-use syntactic_heap::parser::{Grammar};
+use syntactic_heap::parser::{Parser};
 
 #[derive(Copy,Clone,Debug,PartialEq)]
 enum Expr {
@@ -11,7 +11,8 @@ enum Expr {
 
 use Expr::*;
 
-fn reduce(input: &[Expr]) -> Option<(usize,Expr)> {
+// ... '(' ')' ==> '*'
+fn rule1(input: &[Expr]) -> Option<(usize,Expr)> {
     let n = input.len();
     //
     if n >= 2 {
@@ -22,17 +23,31 @@ fn reduce(input: &[Expr]) -> Option<(usize,Expr)> {
 	    (Match,Match) => { return Some((2,Match)); }
 	    _ => {}
 	}
-	//
-	if n >= 3 {
-	    let w3 = input[n-3];
-	    match (w3,w2,w1) {
-		(Left,Match,Right) => { return Some((3,Match)); }
-		_ => {}	    
-	    }
-	}	
     }
-    //
     None
+}
+
+// ... '(' '*' ')' ==> '*'
+fn rule2(input: &[Expr]) -> Option<(usize,Expr)> {
+    let n = input.len();
+    //
+    if n >= 3 {
+	let w1 = input[n-1];	
+	let w2 = input[n-2];	
+	let w3 = input[n-3];
+	match (w3,w2,w1) {
+	    (Left,Match,Right) => { return Some((3,Match)); }
+	    _ => {}	    
+	}
+    }
+    None
+}
+
+fn reduce(input: &[Expr]) -> Option<(usize,Expr)> {
+    match rule1(input) {
+	Some(p) => Some(p),
+	None => rule2(input)	    
+    }
 }
 
 /// =======================================================
@@ -43,7 +58,7 @@ fn reduce(input: &[Expr]) -> Option<(usize,Expr)> {
 fn test_braces_01() {
     let input = vec![Expr::Left];
     // Build grammar
-    let g = Grammar::new(reduce);
+    let g = Parser::new(reduce);
     // Attempt to parse
     assert_eq!(g.parse(&input),input);
 }
@@ -52,7 +67,7 @@ fn test_braces_01() {
 fn test_braces_02() {
     let input = vec![Expr::Right];
     // Build grammar
-    let g = Grammar::new(reduce);
+    let g = Parser::new(reduce);
     // Attempt to parse
     assert_eq!(g.parse(&input),input);
 }
@@ -61,7 +76,7 @@ fn test_braces_02() {
 fn test_braces_03() {
     let input = vec![Expr::Left,Expr::Right];
     // Build grammar
-    let g = Grammar::new(reduce);
+    let g = Parser::new(reduce);
     // Attempt to parse
     assert_eq!(g.parse(&input),vec![Expr::Match]);
 }
@@ -70,7 +85,7 @@ fn test_braces_03() {
 fn test_braces_04() {
     let input = vec![Expr::Left,Expr::Right,Expr::Left];
     // Build grammar
-    let g = Grammar::new(reduce);
+    let g = Parser::new(reduce);
     // Attempt to parse
     assert_eq!(g.parse(&input),vec![Expr::Match,Expr::Left]);
 }
@@ -79,7 +94,7 @@ fn test_braces_04() {
 fn test_braces_05() {
     let input = vec![Expr::Left,Expr::Right,Expr::Right];
     // Build grammar
-    let g = Grammar::new(reduce);
+    let g = Parser::new(reduce);
     // Attempt to parse
     assert_eq!(g.parse(&input),vec![Expr::Match,Expr::Right]);
 }
@@ -88,7 +103,7 @@ fn test_braces_05() {
 fn test_braces_06() {
     let input = vec![Expr::Left,Expr::Right,Expr::Left, Expr::Right];
     // Build grammar
-    let g = Grammar::new(reduce);
+    let g = Parser::new(reduce);
     // Attempt to parse
     assert_eq!(g.parse(&input),vec![Expr::Match]);
 }
@@ -97,7 +112,7 @@ fn test_braces_06() {
 fn test_braces_07() {
     let input = vec![Expr::Left,Expr::Left,Expr::Right, Expr::Right];
     // Build grammar
-    let g = Grammar::new(reduce);
+    let g = Parser::new(reduce);
     // Attempt to parse
     assert_eq!(g.parse(&input),vec![Expr::Match]);
 }
